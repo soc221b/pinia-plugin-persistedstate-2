a0189435a7c5584295570ff75212e1a26a819eb4
Ernest <iendeavor.github.io@gmail.com>
2021-12-21 08:48:52 +0800

    refactor: reduce bundle size

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,118 +1,153 @@
-import {
+import * as shvl from 'shvl'
+
+import type {
   PiniaPlugin,
   PiniaPluginContext,
   StateTree,
-  Store,
-  SubscriptionCallbackMutation,
+  SubscriptionCallback,
 } from 'pinia'
-import * as shvl from 'shvl'
-import { CommonOptions, IStorage } from '.'
-import { PluginOptions } from './type'
-import { identity, noop } from './util'
+import type { PluginOptions, CommonOptions } from './type'
 
-function getOption<K extends keyof CommonOptions<StateTree, Store>>(
-  pluginOptions: CommonOptions<StateTree, Store>,
-  options: CommonOptions<StateTree, Store>,
-  key: K,
-  fallback: CommonOptions<StateTree, Store>[K],
-) {
-  return (options[key] ?? pluginOptions[key] ?? fallback) as Required<
-    CommonOptions<StateTree, Store>
-  >[K]
+function defaultTo<T>(a: T | null | undefined, b: T) {
+  return a != null ? a : b
 }
 
-export const createPersistedStatePlugin = <S extends StateTree, Store>(
-  options?: PluginOptions<S, Store>,
-): PiniaPlugin => {
-  const defaultStorage: IStorage =
-    typeof window === 'object'
-      ? window.localStorage
-      : {
-          getItem: () => null,
-          setItem: () => {},
-          removeItem: () => {},
-        }
-  const defaultAssertStorage = (
-    storage: Required<CommonOptions<S, Store>>['storage'],
-  ) => {
-    const uniqueKey = '@@'
-    const result = storage.setItem(uniqueKey, '1')
-    if (result instanceof Promise) {
-      result.then(() => storage.removeItem(uniqueKey))
-    } else {
-      storage.removeItem(uniqueKey)
-    }
-  }
+function getOption<T, K extends keyof T>(
+  fallback: T[K],
+  key: K,
+  options1: T,
+  options2: T,
+) {
+  return defaultTo(
+    defaultTo(options1[key], options2[key]),
+    fallback,
+  ) as Required<T>[K]
+}
 
-  const pluginOptions = options ?? ({} as PluginOptions<S, Store>)
+export function createPersistedStatePlugin<S extends StateTree, Store>(
+  options?: PluginOptions<S, Store>,
+): PiniaPlugin {
+  const pluginOptions = options || ({} as PluginOptions<S, Store>)
 
   function plugin(context: PiniaPluginContext) {
-    // initialize custom properties
-    let resolveIsReady = () => {}
-    const isReadyPromise = new Promise<void>((resolve) => {
-      resolveIsReady = resolve
-    })
-
-    let pendingCount = 0
-    context.store.$persistedState = {
-      isReady: () => isReadyPromise,
-      pending: false,
-    }
-
     // normalize
-    const options = context.options?.persistedState ?? {}
-    if (getOption(pluginOptions, options, 'persist', true) === false) return
+    const options = (function () {
+      try {
+        return context.options.persistedState || {}
+      } catch {
+        return {}
+      }
+    })()
+    if (getOption(true, 'persist', options, pluginOptions) === false) return
 
-    const key = options.key ?? context.store.$id
-    const overwrite = getOption(pluginOptions, options, 'overwrite', false)
-    const storage = getOption(pluginOptions, options, 'storage', defaultStorage)
-    const filter = getOption(pluginOptions, options, 'filter', () => true)
-    const serialize = getOption(
-      pluginOptions,
+    const key = getOption(context.store.$id, 'key', options, {})
+    const overwrite = getOption(false, 'overwrite', options, pluginOptions)
+    const storage = getOption(
+      (function () {
+        try {
+          return window.localStorage
+        } catch {
+          return {
+            getItem: function () {},
+            setItem: function () {},
+            removeItem: function () {},
+          }
+        }
+      })(),
+      'storage',
       options,
-      'serialize',
+      pluginOptions,
+    )
+    const filter = getOption(
+      function () {
+        return true
+      },
+      'filter',
+      options,
+      pluginOptions,
+    )
+    const serialize = getOption(
       JSON.stringify,
+      'serialize',
+      options,
+      pluginOptions,
     )
     const deserialize = getOption(
-      pluginOptions,
-      options,
-      'deserialize',
       JSON.parse,
+      'deserialize',
+      options,
+      pluginOptions,
     )
-    const migrate = options.migrate ?? identity
+    const migrate = getOption(
+      function <T>(_: T) {
+        return _
+      },
+      'migrate',
+      options,
+      {},
+    )
 
     if (__DEV__ || __TEST__) {
-      ;(options.assertStorage ?? defaultAssertStorage)(storage)
+      if (options.assertStorage === void 0) {
+        options.assertStorage = function (
+          storage: Required<CommonOptions<S, Store>>['storage'],
+        ) {
+          const uniqueKey = '@@'
+          const result = storage.setItem(uniqueKey, '1')
+          const removeItem = function () {
+            storage.removeItem(uniqueKey)
+          }
+          if (result instanceof Promise) {
+            result.then(removeItem)
+          } else {
+            removeItem()
+          }
+        }
+      }
+      options.assertStorage(storage)
     }
 
     // hydrate
-    try {
-      const patchOrOverwrite = (state: any) => {
-        ;(options.beforeHydrate ?? noop)(context.store.$state)
-        if (overwrite) {
-          context.store.$state = state
+    // initialize custom properties
+    let resolveIsReady: Function
+    const isReadyPromise = new Promise<void>(function (resolve) {
+      resolveIsReady = resolve
+    })
+    let pendingCount = 0
+    context.store.$persistedState = {
+      isReady: function () {
+        return isReadyPromise
+      },
+      pending: false,
+    }
+
+    function patchOrOverwrite(state: any) {
+      ;(options.beforeHydrate || function () {})(context.store.$state)
+      if (overwrite) {
+        context.store.$state = state
+      } else {
+        context.store.$patch(state)
+      }
+      resolveIsReady()
+    }
+
+    function parse(value: any) {
+      if (value != null) {
+        const state = deserialize(value)
+        const migrateState = migrate(state)
+
+        if (migrateState instanceof Promise) {
+          migrateState.then(patchOrOverwrite)
         } else {
-          context.store.$patch(state)
+          patchOrOverwrite(migrateState)
         }
+      } else {
         resolveIsReady()
       }
+    }
 
-      const parse = (value: any) => {
-        if (value != null) {
-          const state = deserialize(value)
-          const migrateState = migrate(state)
-
-          if (migrateState instanceof Promise) {
-            migrateState.then(patchOrOverwrite)
-          } else {
-            patchOrOverwrite(migrateState)
-          }
-        } else {
-          resolveIsReady()
-        }
-      }
-
-      let value = storage.getItem(key)
+    try {
+      const value = storage.getItem(key)
       if (value instanceof Promise) {
         value.then(parse)
       } else {
@@ -120,51 +155,43 @@ export const createPersistedStatePlugin = <S extends StateTree, Store>(
       }
     } catch (error) {
       if (__DEV__ || __TEST__) console.warn(error)
-      resolveIsReady()
+      resolveIsReady!()
     }
 
     // persist
-    context.store.$subscribe(
-      (
-        mutation: SubscriptionCallbackMutation<StateTree>,
-        state: {
-          [x: string]: any
-          [x: number]: any
-          [x: symbol]: any
-        },
-      ) => {
-        if (filter(mutation, state) === false) return
+    const callback: SubscriptionCallback<S> = function (mutation, state) {
+      if (filter(mutation, state) === false) return
 
-        if (Array.isArray(options.includePaths)) {
-          state = options.includePaths.reduce((partialState, path) => {
-            return shvl.set(
-              partialState,
-              path,
-              shvl.get(state as Record<string, unknown>, path),
-            )
-          }, {})
-        }
-        if (Array.isArray(options.excludePaths)) {
-          state = deserialize(serialize(state))
-          options.excludePaths.forEach((path) => {
-            return shvl.set(state, path, undefined)
-          }, {})
-        }
+      if (Array.isArray(options.includePaths)) {
+        state = options.includePaths.reduce(function (partialState, path) {
+          return shvl.set(
+            partialState,
+            path,
+            shvl.get(state as Record<string, unknown>, path),
+          )
+        }, {} as any)
+      }
+      if (Array.isArray(options.excludePaths)) {
+        state = deserialize(serialize(state))
+        options.excludePaths.forEach(function (path) {
+          return shvl.set(state, path, void 0)
+        }, {})
+      }
 
-        const value = serialize(state)
-        const result = storage.setItem(key, value)
-        if (result instanceof Promise) {
-          ++pendingCount
-          context.store.$persistedState.pending = pendingCount !== 0
-          result
-            .catch(() => {})
-            .finally(() => {
-              --pendingCount
-              context.store.$persistedState.pending = pendingCount !== 0
-            })
-        }
-      },
-    )
+      const value = serialize(state)
+      const result = storage.setItem(key, value)
+      if (result instanceof Promise) {
+        ++pendingCount
+        context.store.$persistedState.pending = pendingCount !== 0
+        result
+          .catch(function () {})
+          .finally(function () {
+            --pendingCount
+            context.store.$persistedState.pending = pendingCount !== 0
+          })
+      }
+    }
+    context.store.$subscribe(callback)
   }
 
   return plugin
--- src/util.ts
+++
@@ -1,3 +0,0 @@
-export const identity = <T>(_: T) => _
-
-export const noop = (..._: any) => void 0
