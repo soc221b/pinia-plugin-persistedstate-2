446c37cdb3941d2afebb2d5dc6fb6624287c40a3
Ernest <iendeavor.github.io@gmail.com>
2022-08-14 16:49:16 +0800

    docs: add vite-plugin-ssr-example
    closes: #32

---
+++ examples/vite-plugin-ssr-example/pages/index.page.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Pinia Example</h1>
+  <span>Counter that keeps its state on navigation</span>:
+  <button type="button" @click="counterStore.increment">
+    Counter {{ count }}
+  </button>
+</template>
+
+<script setup>
+import { storeToRefs } from 'pinia'
+import { useCounter } from '../stores/useCounter'
+
+const counterStore = useCounter()
+const { count } = storeToRefs(counterStore)
+</script>
